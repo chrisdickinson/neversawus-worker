170cff4a76f414d0a4eea4d0cc9496ca1c988886
Chris Dickinson <christopher.s.dickinson@gmail.com>
2018-12-04 23:59:20 -0800

    always include the release info

--- main.js
+++ main.js
@@ -13,14 +13,10 @@ async function main (event) {
   const response = await fetch(event.request)
   const headers = new Headers(response.headers)
 
-  const userAgent = String(event.request.headers.get('user-agent'))
-  if (userAgent.includes(process.env.SHARED_SECRET)) {
-    headers.set('meta', JSON.stringify({
-      release: process.env.RELEASE
-    }))
-  }
-
   headers.set('x-clacks-overhead', 'GNU Terry Pratchett')
+  headers.set('meta', JSON.stringify({
+    release: process.env.RELEASE
+  }))
 
   return new Response(response.body, {
     statusText: response.statusText,
