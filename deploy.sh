ca176819eb74f054db3c0d90b2adeeb0beacd549
Chris Dickinson <christopher.s.dickinson@gmail.com>
2018-12-04 23:50:54 -0800

    maybe fix the travis issue?

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 set -e
 
+head=$(git rev-parse HEAD)
+target=$(git rev-parse origin/build)
+
 npm ci
 npm t
 webpack -o dist/main.js
+
+if [ "$head" != "$target" ]; then
+  echo "skipping: target is not build"
+  exit
+fi
+
 aws s3 cp dist/main.js s3://cloudflare-workers/neversawus.js --content-type=text/javascript
