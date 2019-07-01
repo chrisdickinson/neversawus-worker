40befa6af0b2a054e89dd621cb31fd705d318264
Chris Dickinson <christopher.s.dickinson@gmail.com>
2019-06-30 22:09:10 -0700

    feat: render framing if given

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const hbs = require('handlebars')
+
 addEventListener('fetch', event => {
   event.respondWith(main(event).then(
     res => res,
@@ -9,16 +11,52 @@ addEventListener('fetch', event => {
   ))
 })
 
+const META_PREFIX = 'x-amz-meta-'
+
+async function getRenderer (framing) {
+  const response = await fetch(`_framing/${framing}`)
+  if (!response.ok) {
+    if (framing === 'default.html') {
+      return (context, body) => body + '<h1>(framing not found)</h1>'
+    } else {
+      return getRenderer('default.html')
+    }
+  }
+
+  const tpl = hbs.compile(await response.text())
+  return (context, body) => {
+    return tpl({ body, ...context })
+  }
+}
+
 async function main (event) {
   const response = await fetch(event.request)
-  const headers = new Headers(response.headers)
+
+  let body = response.body
+  if (response.ok) {
+    const context = [...response.headers].filter(
+      ([key]) => key.startsWith(META_PREFIX)
+    ).reduce((acc, [key, value]) => {
+      acc[key.replace(META_PREFIX, '')] = value
+      return acc
+    }, {})
+
+    if (context.framing) {
+      const render = await getRenderer(context.framing)
+      body = render(context, await response.text())
+    }
+  }
+
+  const headers = new Headers([
+    ...response.headers.filter(([key]) => !key.startsWith(META_PREFIX))]
+  )
 
   headers.set('x-clacks-overhead', 'GNU Terry Pratchett')
   headers.set('meta', JSON.stringify({
     release: process.env.RELEASE
   }))
 
-  return new Response(response.body, {
+  return new Response(body, {
     statusText: response.statusText,
     status: response.status,
     headers
